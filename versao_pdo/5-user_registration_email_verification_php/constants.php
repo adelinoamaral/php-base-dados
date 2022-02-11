6be1ce8565ea2b711ac58b372cb9cce021454e94
Adelino Amaral <p389@esenviseu.net>
2022-02-11 09:52:34 +0000

    Adicionou a aplicação para registar e verificar email

---
+++ versao_pdo/5-user_registration_email_verification_php/constants.php
@@ -0,0 +1,9 @@
+<?php
+define('MAIL_HOST', 'smtp.gmail.com');  // servidor de FTP
+define('MAIL_PORT', 587);   // or port 465
+define('MAIL_USERNAME', 'ticmat@gmail.com');
+define('MAIL_PASSWORD', 'Lino@1965');
+define('MAIL_FROM', 'ticmat@gmail.com');
+define('MAIL_REMETENTE', 'Reset à Senha');
+
+?>
