382c0eb6ab20df7106db4f81c3149b92e0ad57cf
Adelino Amaral <p389@esenviseu.net>
2022-02-12 08:31:06 +0000

    atualização credenciais

--- versao_pdo/5-user_registration_email_verification_php/constants.php
+++ versao_pdo/5-user_registration_email_verification_php/constants.php
@@ -2,7 +2,7 @@
 define('MAIL_HOST', 'smtp.gmail.com');  // servidor de FTP
 define('MAIL_PORT', 587);   // or port 465
 define('MAIL_USERNAME', 'ticmat@gmail.com');
-define('MAIL_PASSWORD', 'Lino@1965');
+define('MAIL_PASSWORD', 'xxxxx');
 define('MAIL_FROM', 'ticmat@gmail.com');
 define('MAIL_REMETENTE', 'Reset à Senha');
 
